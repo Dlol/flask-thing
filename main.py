49975d029bbd692907829dd8e4bb069fbce14324
DravenNadas2 <>
2021-09-03 16:48:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, escape, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return render_template('index.html', name=name)
+
+@app.route('/wow/<name>')
+def wow(name):
+    print(name)
+    if(name == "__name__"):
+      name = __name__
+    return render_template('wow.html', name=name)
+    
+@app.errorhandler(404)
+def lol(what):
+  return render_template('404.html', error=what)
+  
+@app.route('/queue')
+def queue():
+  return render_template('queue.html')
+    
+app.run(host='0.0.0.0', port=8080, debug=True)
